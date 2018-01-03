9d00d6d176474fd08097ac35482ffb4fe869b8ec
Andjii <ander.jim64@gmail.com>
2018-01-03 12:16:13 +0100

    Ajout de la V2
    Ajout du nombre de composantes d'images dans le descripteur

---
+++ Indexeur_ImageV2.h
@@ -0,0 +1,13 @@
+//Developpeur: Ander Jimenez Garcia
+//Date: 09/12/2017
+
+typedef struct descp_img
+{
+	char nb_ID[20];
+	int tab[32768];
+	int nb_comp;
+}type_desc_img;
+
+int quantificateur(int val, int nb_matrice_composante);
+type_desc_img histogramme(int ** matrice_RGB, int taille_lig, int taille_col, int val_max, char chemin[]);
+type_desc_img indexeur_img(char chemin[]);
