fe9b116a753e4493324829b648330a0d58ad27ba
ProgramPenguin <34230836+ProgramPenguin@users.noreply.github.com>
2018-01-30 01:30:04 +0100

    maj bug

--- action_config.c
+++ action_config.c
@@ -1,13 +1,29 @@
 #include "action_config.h"
 
-int lecture_config(type_config * config)
+void lecture_config(type_config * config)
 {
-	FILE * fichier = NULL;
-	char variable[25];
+	/* Cette fonction permet de recuperer les informations necessaires a l'execution du programme
+	Elle permet de recuperer ces parametres depuis un fichier externe : config.txt
+	Pour que cette fonction marche, le .config doit absolument avoir cette forme :
+	
+	<CONFIG>
+	<INDEXEUR_TEXTE> TAILLE_MOT 50 TAILLE_DESC 10 </INDEXEUR_TEXTE>
+	<INDEXEUR_IMAGE> BIT_QUANT 5 </INDEXEUR_IMAGE>
+	<COMPARATEUR> DEG_RESS_IM 21.10 DEG_RESS_TEXT 1.50 </COMPARATEUR>
+	</CONFIG>
+	
+	Les valeurs entieres apres les noms des variables correspondantes peuvent etre changes depuis
+	n'importe quel editeur de texte, et ce afin de modifier le fonctionnement du logiciel.
+	
+	Le balisage permet une lecture plus facile aussi bien pour la machine que pour l'Homme.
+	*/
+	
+	FILE * fichier = NULL; //va contenir le contenu du fichier config le temps du traitement.
+	char variable[25]; //va permettre de stocker la chaine a traiter extraite du fichier config
 	
 	fichier = fopen("config.txt","r");
 	
-	if(fichier != NULL)
+	if(fichier != NULL) //si le fichier c'est correctement ouvert
 	{
 		fscanf(fichier,"%s",variable);
 		if(strcmp(variable, "<CONFIG>")==0)
@@ -21,7 +37,6 @@ int lecture_config(type_config * config)
 					while(strcmp(variable, "</INDEXEUR_TEXTE>")!=0)
 					{
 						fscanf(fichier,"%s",variable);
-						printf("%s\n",variable);
 						if(strcmp(variable, "TAILLE_MOT")==0)
 							fscanf(fichier,"%d",&(config -> taille_mot));
 						
@@ -55,11 +70,12 @@ int lecture_config(type_config * config)
 			}
 		}
 		else
-			fprintf(stderr,"FORMAT NON RESPECTE\n");
+			fprintf(stderr,"FORMAT NON RESPECTE\n"); //format de .config non respecte
 	}
 	else
 	{
 		fprintf(stderr,"ERREUR LECTURE CONFIG\n");
 	}
+	fclose(fichier);
 	
 }
