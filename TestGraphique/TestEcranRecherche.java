6e23161d3ce72cbd01825b9685a700e62917f29f
ProgramPenguin <34230836+ProgramPenguin@users.noreply.github.com>
2018-04-15 17:39:11 +0200

    v2 pour test

--- TestGraphique/TestEcranRecherche.java
+++ TestGraphique/TestEcranRecherche.java
@@ -1,10 +1,11 @@
 package TestGraphique;
 
 import vueGraphique.FenRech;
+import vueGraphique.TestFrameAnder;
 
 public class TestEcranRecherche {
 
 	public static void main(String[] args) {
-		new FenRech();
+		new TestFrameAnder();
 	}
 }
